0720c6a33dc9c679d3860a289f4023a095ae4600
lars <lars@kloud.software>
2018-10-12 10:09:45 +0200

    build

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,93 @@
-extern crate job_scheduler;
+extern crate cron;
+extern crate chrono;
+
+use std::env;
 use std::process::Command;
-use job_scheduler::{JobScheduler, Job};
 use std::time::Duration;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+use std::borrow::Cow;
+
+use cron::Schedule;
+use chrono::Utc;
+use std::str::FromStr;
+use chrono::prelude::*;
+
+struct Cronproc<'a> {
+    cronstr: Cow<'a, str>,
+    cronprogram: Cow<'a, str>,
+    cronargs: Cow<'a, str>,
+}
 
 fn main() {
-    let mut sched = JobScheduler::new();
+    let path = env::home_dir().unwrap().to_str().unwrap().to_owned() + "/.larscrontab";
+    let f = File::open(path).expect("~/.larscrontab file is missing");
+    let f = BufReader::new(f);
+    let mut cron_vec: Vec<Cronproc> = Vec::new();
 
-    sched.add(Job::new("5/10 * * * * *".parse().unwrap(), || {
-        Command::new("bash")
-            .arg("/home/lars/.scripts/bin/newsboat-reload")
-            .spawn()
-            .expect("sh command failed to start");
-    }));
+    for line in f.lines() {
+        let line_str = line.unwrap();
+        let mut iter = line_str.split_whitespace();
+
+        let mut cron_str = String::new();
+        for i in 0..6 {
+            cron_str += iter.next().unwrap();
+            if i != 5 {
+                cron_str += " ";
+            }
+        }
+
+        let program = iter.next().unwrap();
+        let arg = iter.next().unwrap();
+
+
+        let cronprc = Cronproc {
+            cronstr: Cow::Owned(cron_str.to_owned()),
+            cronprogram: Cow::Owned(program.to_owned()),
+            cronargs: Cow::Owned(arg.to_owned()),
+        };
+
+        cron_vec.push(cronprc);
+    }
+
 
     loop {
-        sched.tick();
+        let sleep_timer = next_min_sleep(&cron_vec);
+        let mut sleep:u64 = 0;
+        if sleep_timer - 100 >= 0 {
+            sleep = (sleep - 100) as u64;
+        }
 
-        std::thread::sleep(Duration::from_millis(500));
+        std::thread::sleep(Duration::from_millis(sleep));
+
+        exec_cron(&cron_vec);
+    }
+}
+
+fn get_timer_from_cron(cron_item: &Cronproc) -> i64 {
+    let utc: DateTime<Utc> = Utc::now();
+    let schedule = Schedule::from_str(&cron_item.cronstr).unwrap();
+    let timer = (schedule.upcoming(Utc).take(1).next().unwrap().timestamp() - utc.timestamp());
+    timer
+}
+
+fn next_min_sleep(crons: &Vec<Cronproc>) -> i64 {
+    let mut timers: Vec<i64> = Vec::new();
+    for cron_item in crons {
+        timers.push(get_timer_from_cron(cron_item));
+    }
+
+    *timers.iter().min().unwrap()
+}
+
+fn exec_cron(crons: &Vec<Cronproc>) {
+    for cron_item in crons {
+        if get_timer_from_cron(cron_item) <= 60 {
+            Command::new(cron_item.cronprogram.to_string())
+                .arg(cron_item.cronargs.to_string())
+                .spawn()
+                .expect("sh command failed to start"); 
+        }
     }
 }
